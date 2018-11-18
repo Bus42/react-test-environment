9bee4c2690e1f23c7ebd17eb18c3b0b015a2606f
Bus42 <bus42@studio42dev.com>
2018-11-18 11:21:14 -0700

    Gets Request component working

--- src/components/Request.js
+++ src/components/Request.js
@@ -5,30 +5,69 @@ class Request extends Component {
     super(props);
     this.state = {
       data: "...loading",
-      url: "http://www.filltext.com?rows=10&f={firstName}"
+      base_url: "http://www.filltext.com",
+      params: ["rows=10", "name={firstName}"],
+      final_url: ""
     };
   }
 
+  buildURL = () => {
+    let newURL = [this.state.base_url];
+    if (this.state.params.length > 0) {
+      newURL.push("?");
+      for (let i = 0; i < this.state.params.length; i++) {
+        console.log(this.state.params[i]);
+        if (
+          this.state.params[i + 1] !== null &&
+          this.state.params[i + 1] !== undefined
+        ) {
+          newURL.push(`${this.state.params[i]}&`);
+        } else {
+          newURL.push(this.state.params[i]);
+        }
+      }
+    }
+    let final_url = newURL.join("");
+    this.setState({ final_url });
+  };
+
+  componentWillMount() {
+    this.buildURL();
+  }
+
   componentDidMount() {
+    this.makeRequest();
+  }
+
+  makeRequest = () => {
     let r = new XMLHttpRequest();
-    r.open("GET", this.state.url, true);
+    r.open("GET", this.state.final_url, true);
     r.onreadystatechange = () => {
       if (r.readyState !== 4 || r.status !== 200) return;
       var data = JSON.parse(r.responseText);
       this.setState({ data });
     };
     r.send();
-  }
+  };
 
   render() {
-    let divStyle = {backgroundColor:'#424242',color: '#AFAFAF', minHeight:'100vh', width: '100vw',margin:'0',padding:'20px',boxSizing:'border-box'};
+    let divStyle = {
+      backgroundColor: "#424242",
+      color: "#AFAFAF",
+      minHeight: "100vh",
+      width: "100vw",
+      margin: "0",
+      padding: "20px",
+      boxSizing: "border-box"
+    };
     return (
       <div style={divStyle}>
-        <div><pre>{JSON.stringify(this.state.data, null, 2)}</pre></div>
+        <div>
+          <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
+        </div>
       </div>
     );
   }
 }
 
 export default Request;
- 
